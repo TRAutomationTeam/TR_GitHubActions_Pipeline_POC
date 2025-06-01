76eea506a7476a9c26fc22490a1359355f6f25f4
rajesh.karumuru@thomsonreuters.com <Puggie123456$>
2025-06-01 23:13:43 +0530

    Incorporated Test Case Palindrome

---
+++ .codedworkflows/ConnectionsFactory.cs
@@ -0,0 +1,6 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace TR_GitHubActions_Pipeline_POC
+{
+}
---
+++ .codedworkflows/ConnectionsManager.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace TR_GitHubActions_Pipeline_POC
+{
+    public class ConnectionsManager
+    {
+        public ConnectionsManager(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
